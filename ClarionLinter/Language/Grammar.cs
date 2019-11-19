600e36789f58c9f92523168739090a8b7e0fc110
Benjamin Sladewski <benjaminsladewski@frazer.biz>
2019-11-19 13:32:52 -0500

    Consolidated a few files and started building up the parser.
    Token types are now organized under the LexicalSpec.cs file.
    Defined the basic types that make up a Grammar.
    Renamed the XML tags in the Grammar.xml file.
    Stubbed out an IParser interface and DefaultParser class.

--- ClarionLinter/Language/Grammar.cs
+++ ClarionLinter/Language/Grammar.cs
@@ -11,5 +11,87 @@ namespace Language
     public class Grammar
     {
 
+        /// <summary>
+        /// A list of RuleDefinitions specified by the grammar.
+        /// </summary>
+        [XmlElement("ruleDefinition")]
+        public RuleDefinition[] RuleDefinitions;
+
     }
+
+    /// <summary>
+    /// A RuleDefinition defines abstract syntax made up of Rules and Sequences.
+    /// </summary>
+    [XmlType("ruleDefinition")]
+    public class RuleDefinition
+    {
+
+        /// <summary>
+        /// A display name for this RuleDefinition.
+        /// </summary>
+        [XmlAttribute("name")]
+        public string Name;
+
+        /// <summary>
+        /// Rules reference the RuleDefinitions and Terminal tokens that make up the syntax of
+        /// this RuleDefinition. Each Rule is treated as a different option. That is, if the Rules
+        /// are defined as [foo, bar, baz], this RuleDefinition will match any single occurance of
+        /// foo, bar, or baz.
+        /// </summary>
+        [XmlElement("rule", typeof(Rule))]
+        [XmlElement("terminal", typeof(Token))]
+        public Rule[] Rules;
+
+        /// <summary>
+        /// Sequences indicate that this RuleDefinition may match a number of Rules in a specific
+        /// order. Like Rules, Sequences are treated as different options. The contents of a
+        /// Sequence, however, must all occur in the order they are specified. For example, a
+        /// sequence defined by [foo, bar, baz] will match an occurance of foo followed by an
+        /// occurance of bar followed by an occurance of baz.
+        /// </summary>
+        [XmlElement("sequence")]
+        public Sequence[] Sequences;
+
+    }
+
+    /// <summary>
+    /// A Sequence is similar to a RuleDefinition in that it stores a list of rules. The Sequence,
+    /// however, indicates that all rules must occur in the order they were specified.
+    /// </summary>
+    [XmlType("sequence")]
+    public class Sequence : RuleDefinition { }
+
+    /// <summary>
+    /// Rules are matched during parsing to define abstract syntax.
+    /// </summary>
+    public class Rule
+    {
+
+        /// <summary>
+        /// A display name for this Rule.
+        /// This value is also used to relate the Rule to its definition by name. For example, a
+        /// Rule will use the Name field to determine if this Rule matches a given RuleDefinition
+        /// or a Terminal will use the Name field to determine if this Rule matches a given Token.
+        /// </summary>
+        [XmlAttribute("name")]
+        public string Name;
+
+    }
+
+    /// <summary>
+    /// A Terminal Rule matches a Token. Terminal Rules cannot be expanded further and thus
+    /// represent a leaf node in the abstract syntax tree.
+    /// </summary>
+    public class Terminal : Rule
+    {
+
+        /// <summary>
+        /// A type of Token matched by this Terminal Rule. For example, a type of literal will
+        /// match any Tokens that are an instance of the Literal Token type.
+        /// </summary>
+        [XmlAttribute("type")]
+        public string Type;
+
+    }
+
 }
---
+++ ClarionLinter/Language/Parser.cs
@@ -0,0 +1,25 @@
+﻿namespace ClarionLinter.Language
+{
+
+    /// <summary>
+    /// IParser represents an abstract syntax tree.
+    /// </summary>
+    public interface IParser
+    {
+
+        // TODO: I want to be able implement IParser to create decorators that will be used to
+        // 
+
+    }
+
+    /// <summary>
+    /// A DefaultParser parses a programming language from a stream of Lexemes.
+    /// </summary>
+    public class DefaultParser : IParser
+    {
+
+        // TODO: Implement IParser with a multi-pass bottom-up parsing algorithm.
+
+    }
+
+}
